ae9f01ee930c021daff4ce38bc8c3ca5975d163d
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-03-07 23:38:57 +0300

    added to generics

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,5 +1,23 @@
 package generics;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("Privet");
+        list.add(6);
+        list.add(new StringBuilder("ok"));
+        list.add(new Car());
+
+        for (Object o : list) {
+            System.out.println(o + " length " + ((String)o).length()); // здесь ошибка! "ClassCastException"
+//             на уровне компиляции ее не видно, но на этапе запуска она возникнет
+//             это одна из двух причин существования дженериков
+        }
+    }
+}
+
+class Car {
 }
