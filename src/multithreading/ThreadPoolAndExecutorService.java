35320c41ce3c9d51ffb167a082385178a49ef397
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-05-01 23:09:08 +0300

    added to ThreadPoolAndExecutorService

--- src/multithreading/ThreadPoolAndExecutorService.java
+++ src/multithreading/ThreadPoolAndExecutorService.java
@@ -23,9 +23,9 @@ Thread pool, помимо того, что он удобен для исполь
 
 Thread pool - это множество потоков и допустим, что в thread pool находятся три потока: Т1, Т2, Т3. Программа поставляет
 thread pool разные задания: task1, task2, ... , taskN. Уже внутри себя thread pool сам регулирует какой поток будет
-выполнять какое задание. Допустим, за task1 может взяться поток T1, за task2 может взяться поток Т3. Когда поток Т1
+выполнять какое задание. Допустим, за task1 может взяться поток T1, за task2 может взяться поток Т3 и тд. Когда поток Т1
 заканчивает выполнение task1, он "говорит": "я свободен и могу выполнить следующее задание". Когда какой-либо поток
-освобождается и если есть ожидающие задания, то освободившийся thread берет ожидающее одно задание и начинает его
+освобождается и если есть ожидающие задания, то освободившийся thread берет одно ожидающее задание и начинает его
 выполнять. Вот и вся концепция Thread pool. В Java происходит работа с Thread pool по средствам объектов типа Executor
 (c англ. исполнитель). Executor это интерфейс.
 В Java c thread pool-ами удобнее всего работать посредством ExecutorService.
@@ -41,13 +41,14 @@ public class ThreadPoolAndExecutorService {
         один из factory (с англ. фабрика) методов класса Executors.
          */
         ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
         /*
         Thread pool удобнее всего создавать, используя factory методы класса Executors:
 
         Executors.newFixedThreadPool(int count) - создаст pool с count (с конкретным значением) потоков
         newFixedThreadPool - новый фиксированный пул потоков;
 
-        Executors.SingleThreadExecutor() - создаст pool с одним потоком
+        Executors.SingleThreadExecutor() - создаст pool с одним потоком;
 
         Для работы с Thread pool используется ExecutorService, а создавать Thread pool лучше всего с помощью класса
         Executors. В примере выше Thread pool создан не при помощи конструктора, а с помощью метода newFixedThreadPool(5);
@@ -65,6 +66,30 @@ public class ThreadPoolAndExecutorService {
         }
         System.out.println("Thread main ends");
         /*
+                public class ThreadPoolAndExecutorService {
+                    public static void main(String[] args) {
+                        ExecutorService executorService = Executors.newFixedThreadPool(5);
+                        for (int i = 0; i < 10; i++) {
+                            executorService.execute(new RunnableImplementation100()); // execute c англ. выполнять
+                        }
+                        System.out.println("Thread main ends");
+                    }
+                }
+
+                class RunnableImplementation100 implements Runnable {
+
+                    @Override
+                    public void run() {
+                        System.out.println(Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+
         Запуск программы. Вывод:
         Thread main ends
         pool-1-thread-1
@@ -81,7 +106,7 @@ public class ThreadPoolAndExecutorService {
 
         Внутри потока main был создан Thread pool из 5-ти потоков:
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Далее при помощи цикла и метода execute 10 раз было передано задание, а именно new RunnableImplementation100 в
+        Далее при помощи цикла и метода execute 10 раз было передано задание, а именно new RunnableImplementation100() в
         Thread pool и поток main (он сработал быстрее всех) вывел на экран "Thread main ends".
 
         Как происходит работа pool-a?
@@ -140,6 +165,7 @@ public class ThreadPoolAndExecutorService {
         shutdown() (с англ. выключение):
                 public class ThreadPoolAndExecutorService {
                     public static void main(String[] args) {
+                        ExecutorService executorService = Executors.newFixedThreadPool(5);
                         for (int i = 0; i < 10; i++) {
                             executorService.execute(new RunnableImplementation100());
                         }
@@ -162,7 +188,7 @@ public class ThreadPoolAndExecutorService {
         Thread main ends
 
         ExecutorService выполнил все задания и после программа закончилась. Вызвав метод shutdown, даем понять программе,
-        что задач больше не будет и программа завершится после того, как все уже полученные задания выполнятся.
+        что задач больше не будет. Программа завершится после того, как все уже полученные задания выполнятся.
 
         Метод execute передает задание (task) в Thread pool, где оно выполняется одним из потоков.
         После выполнения метода shutdown ExecutorService понимает, что новых заданий больше не будет и, выполнив
@@ -181,15 +207,18 @@ public class ThreadPoolAndExecutorService {
 
                 public class ThreadPoolAndExecutorService {
                     public static void main(String[] args) {
+                        ExecutorService executorService = Executors.newFixedThreadPool(5);
                         for (int i = 0; i < 10; i++) {
                             executorService.execute(new RunnableImplementation100()); // execute c англ. выполнять
                         }
                         executorService.shutdown();
+
                         try {
-                            executorService.awaitTermination(5, TimeUnit.SECONDS);
+                            executorService.awaitTermination(5, TimeUnit.SECONDS);     <------ awaitTermination
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
+
                         System.out.println("Thread main ends");
                     }
                 }
@@ -235,7 +264,7 @@ public class ThreadPoolAndExecutorService {
 
         Метод awaitTermination принуждает поток в котором он вызвался подождать до тех пор, пока не выполнится одно из
         двух событий: либо ExecutorService прекратит свою работу, либо пройдет время, указанное в параметре метода
-        метода awaitTermination.
+        awaitTermination.
 
         Еще один вид Thread pool:
         ExecutorService executorService = Executors.newSingleThreadExecutor();
