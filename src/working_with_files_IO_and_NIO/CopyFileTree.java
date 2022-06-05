00530a5072aa4a4949e2f09ff971c3c167170dff
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-06-05 21:45:11 +0300

    added in CopyFileTree

--- src/working_with_files_IO_and_NIO/CopyFileTree.java
+++ src/working_with_files_IO_and_NIO/CopyFileTree.java
@@ -59,6 +59,44 @@ Path (нужен импорт import java.nio.file.Path;):
         class MyFileVisitorForCopy extends SimpleFileVisitor<Path> {
         }
 
+Как выглядит сам класс SimpleFileVisitor:
+
+            public class SimpleFileVisitor<T> implements FileVisitor<T> {
+
+            protected SimpleFileVisitor() {
+                    }
+
+            @Override
+            public FileVisitResult preVisitDirectory(T dir, BasicFileAttributes attrs) throws IOException {
+                Objects.requireNonNull(dir);
+                Objects.requireNonNull(attrs);
+                return FileVisitResult.CONTINUE;
+            }
+
+
+            @Override
+            public FileVisitResult visitFile(T file, BasicFileAttributes attrs) throws IOException {
+                Objects.requireNonNull(file);
+                Objects.requireNonNull(attrs);
+                return FileVisitResult.CONTINUE;
+            }
+
+
+            @Override
+            public FileVisitResult visitFileFailed(T file, IOException exc) throws IOException {
+                Objects.requireNonNull(file);
+                throw exc;
+             }
+
+            @Override
+            public FileVisitResult postVisitDirectory(T dir, IOException exc) throws IOException {
+                Objects.requireNonNull(dir);
+                if (exc != null)
+                    throw exc;
+                return FileVisitResult.CONTINUE;
+            }
+        }
+
 Для того, чтобы производить процесс копирования, необходимо знать откуда будет копироваться файл или папка и куда будет
 скопировано. Создадим два поля:
 
@@ -122,7 +160,64 @@ destination = /Users/shevchenkostas77/Desktop/CopyHere"
 
 Сначала сработает то, что в скобках (в параметре метода resolve). Метод relativize возвращает относительный путь dir
 относительно абсолютного пути source. Рассмотрим на примере папки "Y1". Когда сработает метод preVisitDirectory для
-папки "Y1"
+папки "Y1", то Path newDestination будет таким:
+
+        /Users/shevchenkostas77/Desktop/CopyHere/.resolve(/Users/shevchenkostas77/Desktop/X
+                .relativize(/Users/shevchenkostas77/Desktop/X/Y))
+
+Метод relativize возвращает относительный путь из двух абсолютных путей, т.е. позволяет вычислить «разницу путей»: один
+путь относительно другого. Т.е. на выходе в скобках метода resolve - source.relativize(dir) будет "Y".
+Метод resolve объединяет два пути в один, т.е. строит новый абсолютный путь из абсолютного и относительного. Т.е.
+объединит два пути /Users/shevchenkostas77/Desktop/CopyHere/ + Y , что в итоге newDestination будет иметь путь:
+
+                Path newDestination = "/Users/shevchenkostas77/Desktop/CopyHere/Y"
+
+Так по антологии со всеми остальными папками.
+Такая же аналогичная работа будет происходить и в методе visitFile.
+
+Код:
+
+public class CopyFileTree {
+    public static void main(String[] args) {
+        Path source = Path.of("/Users/shevchenkostas77/Desktop/X");
+        Path destination = Path.of("/Users/shevchenkostas77/Desktop/CopyHere");
+        try {
+            Files.walkFileTree(source, new MyFileVisitorForCopy(source, destination));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done!");
+    }
+}
+
+class MyFileVisitorForCopy extends SimpleFileVisitor<Path> {
+    Path source; // откуда будет идти копирование
+    Path destination; // куда будет копироваться
+
+    public MyFileVisitorForCopy(Path source, Path destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(dir));
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(file));
+        Files.copy(file, newDestination);
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+Запуск программы. Вывод на экран:
+Done!
+
+
  */
 
 public class CopyFileTree {
